8d43a2abbf562a6c4f21b434618734dcf21b2449
avalos-p <avalospabloa@gmail.com>
2023-11-25 06:54:59 -0300

    cleaning data new functions

--- utils/cleaning.py
+++ utils/cleaning.py
@@ -1,34 +1,27 @@
 import pandas as pd
 from unidecode import unidecode
 
-def cleaning_names(column: pd.Series) -> pd.Series:
-    # name cleaning
+
+def cleaning_letters(column: pd.Series) -> pd.Series:
+    if not isinstance(column, pd.Series):
+        raise ValueError('Error: This function works with pandas series - columns')
     column = column.apply(lambda x: unidecode(x) if pd.notnull(x) else x)
-    column = column.str.lower()  # O file['name'] = file['name'].str.upper()
-    column = column.str.replace('[^a-zA-Z0-9\s]+', '', regex=True).str.strip()
+    column = column.str.lower()
+    column = column.str.replace('[^a-z0-9\s]+', '', regex=True).str.strip()
     return column
 
-def cleaning_prices(column: pd.Series) -> pd.Series:
-   # price cleaning
-    column = column.str.replace('\n', '').str.strip() # TO DO: USD CONVERTION
+def keep_numbers_only(column: pd.Series) -> pd.Series:
+    if not isinstance(column, pd.Series):
+        raise ValueError('Error: This function works with pandas series - columns')
+    column = column.apply(lambda x: str(x))  # Changes values to str
+    column = column.str.replace('[^0-9]+', '', regex=True)
+#    column = pd.to_numeric(column, errors='coerce') # Just in case I want to change empty for NaN
     return column
 
-def cleaning_days(column: pd.Series) -> pd.Series:
-    # days cleaning
-    column = column.replace('por día', '1').replace('por mes', '30')
+def number_of_dayss(column: pd.Series) -> pd.Series:
+    if not isinstance(column, pd.Series):
+        raise ValueError('Error: This function works with pandas series - columns')
+    
+    column = column.replace('por día', '1').replace('por mes', '30').replace('por dia', '1')
     column = column.replace('por quincena', '15').replace('por semana', '7')
     return column
-
-def cleaning_rooms(column: pd.Series) -> pd.Series:
-    # rooms cleaning
-    column = column.str.replace('\n', '').str.replace('dorm.','').str.strip()
-    return column
-
-def cleaning_bathrooms(column: pd.Series) -> pd.Series:
-    # bathrooms cleaning
-    column = column.str.replace(r'\n', '').str.replace('baños','').str.replace('baño','').str.strip()
-
-    return column
-
-
-# capacity cleaning
