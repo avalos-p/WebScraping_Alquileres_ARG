9e035040bd6ff4364c6f4e717f3377c4ea775ad4
avalos-p <avalospabloa@gmail.com>
2023-12-07 05:19:16 -0300

    scrapy files moved to utils

---
+++ utils/spideralquileres_ap.py
@@ -0,0 +1,95 @@
+import scrapy
+from config.cfg import ARGENPROP_WEBSITE,ARGENPROP_PROVINCES
+import datetime
+
+import logging
+
+date = datetime.datetime.now()
+date_formatted = date.strftime("%d-%m-%Y")
+
+class SpideralquileresSpider_AP(scrapy.Spider):
+    name = "spideralquileres_AP"
+    allowed_domains = ARGENPROP_WEBSITE # calling sites from cfg 
+    start_urls = list(ARGENPROP_PROVINCES.values()) #
+    
+    custom_settings = {
+    'FEED_FORMAT': 'csv',
+    'FEED_URI': f'csv/alquileres_ap{date_formatted}.csv' # saving raw data
+    }
+    
+
+    def parse(self, response):
+        houses = response.css('.listing__item') #this is the container for information
+
+        for house in houses:
+            yield{
+                'name' : house.css('.card__address::text').get(),
+                'price': house.xpath('.//p[@class="card__price"]/text()[2]').get(),
+                'days': 'not available',
+                'rooms': house.css('li:has(i.icono-cantidad_dormitorios) span::text').get(),
+                'bathrooms': house.css('li:has(i.icono-cantidad_banos) span::text').get(),
+                'capacity': 'not available',
+                'date': date_formatted,
+                'province': next((provincia for provincia, url in ARGENPROP_PROVINCES.items() if provincia in response.url), None),
+                'site':'argenprop.com',
+                }
+
+
+        
+        next_page =self.allowed_domains[0]+response.css('a[aria-label="Siguiente"]::attr(href)').get()
+
+
+        if next_page:
+            if next_page == self.start_urls[0]:
+                pass
+            else:
+                yield response.follow(next_page, callback=self.parse)
+ # TO DO: solve captcha
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ utils/spideralquileres_pi.py
@@ -0,0 +1,47 @@
+import scrapy
+from config.cfg import PARAIRNOS_WEBSITE,PARAIRNOS_PROVINCES
+import datetime
+
+import logging
+
+date = datetime.datetime.now()
+date_formatted = date.strftime("%d-%m-%Y")
+
+class SpideralquileresSpider_PI(scrapy.Spider):
+    name = "spideralquileres_PI"
+    allowed_domains = PARAIRNOS_WEBSITE
+    start_urls = list(PARAIRNOS_PROVINCES.values())
+
+    custom_settings = {
+    'FEED_FORMAT': 'csv',
+    'FEED_URI': f'csv/alquileres_pi{date_formatted}.csv'# saving raw data
+    }
+    
+
+    def parse(self, response):
+        houses = response.css('.card-horizontal') #this is the container for information
+
+        for house in houses:
+            yield{
+                'name' : house.css('h4.title::text').get(),
+                'price': house.xpath('.//span[@class="price "]/text()[3]').get(),
+                'days': house.css('div.price-sub::text').get(),
+                'rooms': house.css('ul.list-inline.amenities li::text').re_first(r'\d+'),
+                'bathrooms': house.css('ul.list-inline.amenities li::text').re_first(r'\d+\s*Bañ[os]'),
+                'capacity': house.css('ul.list-inline.amenities li::text').re_first(r'\d+\s*Persona[s]'),
+                'date': date_formatted,
+                'province': next((provincia for provincia, url in PARAIRNOS_PROVINCES.items() if provincia in response.url), None),
+                'site':'parairnos.com',
+                }
+
+
+        next_page_list = response.css('ul.pagination li a::attr(href)')
+        next_page = next_page_list[-1]
+
+
+        if next_page:
+            if next_page == self.start_urls[0]:
+                pass
+            else:
+                yield response.follow(next_page, callback=self.parse)
+
